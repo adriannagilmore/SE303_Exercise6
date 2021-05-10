05ff5e29ae3b483ba50585a5d79e55044cd1b1c5
Adrianna Gilmore <a_inman33@yahoo.com>
2021-05-09 17:10:54 -0700

    named more code smells

--- main.rb
+++ main.rb
@@ -1,9 +1,10 @@
 def draw_button(label_text, x, y, foreground_color, is_dark_mode)   #Code Smell: Long Parameter List    Fix: Preserve Whole Object
   if is_dark_mode
     # darken foreground color for dark mode                         #Code Smell: Comments               Fix: Let code speak for itself
-    paint(label_text, x, y, foreground_color - 10, '#111111')
+    paint(label_text, x, y, foreground_color - 10, '#111111')       #Code Smell: Long Parameter List    Fix: Replace Parameter with Query
   else
     # lighten foreground color for non-dark mode                    #Code Smell: Comments               Fix: Let code speak for itself
-    paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+    paint(label_text, x, y, foreground_color + 10, '#E0E0E0')       #Code Smell: Long Parameter List    Fix: Replace Parameter with Query
   end
 end
+
